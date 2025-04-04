6985f3f51737c5204659cbf4a2dedd68a335d109
LimChangeun <cksrms1030@naver.com>
2025-04-04 09:41:48 +0900

    IF-135 LogoSection 구현

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <script setup>
 import { useRoute } from "vue-router";
-import Header from "./pages/common/Header.vue";
-import Sidebar from "./pages/common/Sidebar.vue";
-import { ref } from "vue";
 
 const route = useRoute();
-const isSidebarOpen = ref(false);
 
-const toggleSidebar = () => {
-  isSidebarOpen.value = !isSidebarOpen;
-};
 </script>
 
 <template>
-  <div>
-    <Header @toggle-sidebar="toggleSidebar"></Header>
-    <Sidebar v-if="isSidebarOpen" />
-    
-    <main class="mt-16">
-      <router-view></router-view>
-    </main>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped>

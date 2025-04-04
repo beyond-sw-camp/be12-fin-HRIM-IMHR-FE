09288c7cc36e2652881dca93aa3a09d315cfde56
LimChangeun <cksrms1030@naver.com>
2025-04-04 10:14:07 +0900

    IF-126 로그인페이지 작성

--- src/pages/common/LogoSection.vue
+++ src/pages/common/LogoSection.vue
@@ -2,16 +2,13 @@
 </script>
 
 <template>
-  <div class="w-1/2 bg-gradient-to-b from-blue-500 to-blue-600 text-white flex flex-col justify-center items-center">
-    
-    <h1 class="text-5xl font-bold mb-4">IM HR</h1>
+  <h1 class="text-5xl font-bold mb-4">IM HR</h1>
 
-    <p class="text-lg mt-2">
-      기업의 모든 인재 관리를 간편하게
-      <br />
-      지속가능한 미래를 실현합니다.
-    </p>
-  </div>
+  <p class="text-lg mt-2">
+    기업의 모든 인재 관리를 간편하게
+    <br />
+    지속가능한 미래를 실현합니다.
+  </p>
 </template>
 
 

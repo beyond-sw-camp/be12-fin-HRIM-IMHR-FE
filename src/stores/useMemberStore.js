fb41765072c076cdeec76f94e904cf10b7b063b5
InukChoi <iu.choi3197@gmail.com>
2025-04-14 15:20:14 +0900

    [Feat] 비밀번호 재설정 api 연동

--- src/stores/useMemberStore.js
+++ src/stores/useMemberStore.js
@@ -26,6 +26,10 @@ export const useMemberStore = defineStore('member', {
             const response = await axios.post("/api/member/find-pw", form);
             return response;
         },
+        async changePW(form) {
+            const response = await axios.post("/api/member/reset-pw", form);
+            return response;
+        },
         async companySignup(formData) {
             const response = await axios.post("/api/member/signup/company", formData, {
                 headers: {

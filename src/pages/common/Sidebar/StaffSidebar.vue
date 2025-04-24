cf466b50a283acff6d83d10c0f1b3f0bcdf2e5da
choi-won-ik <whwwhs7837@gmail.com>
2025-04-24 17:49:25 +0900

    [Fix] 파트너 대시보드 위치 수정

--- src/pages/common/Sidebar/StaffSidebar.vue
+++ src/pages/common/Sidebar/StaffSidebar.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { useMemberStore } from '../../../stores/useMemberStore'
+const memberStore = useMemberStore()
 const props = defineProps({
   isOpen: Boolean,
 });
 
 const menuItems = [
-  { path: "/partner", label: "기업 ESG 현황" },
+  { path: `/partner/${memberStore.myCompanyIdx}`, label: "기업 ESG 현황" },
   { path: "/educationList/1", label: "ESG 교육 활동" },
   { path: "/activityList", label: "ESG 기타 활동" },
   { path: "/campaignlist", label: "ESG 사내 캠페인 활동" },
@@ -19,20 +21,17 @@ const menuItems = [
     :class="{
       '-translate-x-full': !isOpen,
       'translate-x-0': isOpen,
-    }"
-  >
+    }">
     <nav class="mt-5">
       <ul class="p-4 space-y-1">
         <li v-for="item in menuItems" :key="item.path">
-          <router-link
-            :to="item.path"
+          <router-link :to="item.path"
             class="block px-4 py-2 rounded-md transition duration-200 hover:bg-slate-700 hover:shadow-md hover:scale-[1.01] border-l-4"
             :class="{
               'border-emerald-400 bg-slate-700 font-semibold':
                 $route.path === item.path,
               'border-transparent': $route.path !== item.path,
-            }"
-          >
+            }">
             {{ item.label }}
           </router-link>
         </li>
